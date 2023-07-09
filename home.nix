ae8a4517afc556d2f814d591d5d69973f84e24ca
pvdvreede <pvdvreede@gmail.com>
2023-07-09 16:04:47 +1000

    add tmux theme and config.

--- home.nix
+++ home.nix
@@ -71,7 +71,6 @@
         autosquash = true;
       };
     };
-
   };
   programs.gh = {
     enable = true;
@@ -113,10 +112,16 @@
     enable = true;
     baseIndex = 1;
     clock24 = true;
+    plugins = with pkgs.tmuxPlugins; [
+      catppuccin
+      tmux-fzf
+    ];
     disableConfirmationPrompt = true;
     terminal = "xterm-256color";
     shell = "${pkgs.fish}/bin/fish";
     extraConfig = ''
+      set -g status-position top
+
       # pneumonic for splitting windows
       bind-key v split-window -h
       bind-key s split-window -v
@@ -125,6 +130,13 @@
       bind-key -r j select-pane -D
       bind-key -r k select-pane -U
       bind-key -r l select-pane -R
+
+      # catppuccin theme config
+      set -g @catppuccin_flavour 'latte'
+      set -g @catppuccin_window_tabs_enabled on
+      set -g @catppuccin_date_time "%Y-%m-%d %H:%M"
+      set -g @catppuccin_host "on"
+      set -g @catppuccin_user "on"
     '';
     keyMode = "vi";
     newSession = true;
