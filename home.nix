0ee5810681f50e22dce3d4158137d3b5d48a969e
pvdvreede <pvdvreede@gmail.com>
2023-07-09 11:44:38 +1000

    Add in tmux and vim plugins and config.

--- home.nix
+++ home.nix
@@ -1,41 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "pvdvreede";
   home.homeDirectory = "/home/pvdvreede";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.stateVersion = "23.05";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-    pkgs.vim
-    pkgs.git
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -64,7 +36,7 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "vim";
   };
 
   # Let Home Manager install and manage itself.
@@ -82,6 +54,37 @@
     shellAbbrs = {
       gst = "git status";
       gco = "git checkout";
+      ga = "git add";
+      gc = "git commit";
     };
+  };
+  programs.vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      ctrlp
+      vim-airline
+      vim-move
+      vim-nix
+    ];
+    settings = {
+      relativenumber = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      expandtab = true;
+    };
+    extraConfig = ''
+      let g:move_key_modifier = 'C'
+      autocmd BufWritePre * :%s/\s\+$//e
+    '';
+  };
+  programs.tmux = {
+    enable = true;
+    baseIndex = 1;
+    clock24 = true;
+    disableConfirmationPrompt = true;
+    extraConfig = "";
+    keyMode = "vi";
+    newSession = true;
+    prefix = "C-Space";
   };
 }
