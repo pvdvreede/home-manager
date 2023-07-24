dd70d434fb27924f5dafc09c1b0e74ba6e715022
pvdvreede <pvdvreede@gmail.com>
2023-07-24 18:46:48 +1000

    vscode setting additions and base64 extension.

--- work.nix
+++ work.nix
@@ -39,23 +39,26 @@
     enableUpdateCheck = false;
     mutableExtensionsDir = false;
     extensions = with vscode-marketplace; [
+      adamhartford.vscode-base64
       bbenoist.nix
       catppuccin.catppuccin-vsc
       hashicorp.terraform
+      johnpapa.vscode-peacock
       ms-azuretools.vscode-docker
       ms-vscode-remote.remote-containers
       oderwat.indent-rainbow
       redhat.vscode-yaml
-      johnpapa.vscode-peacock
     ];
     userSettings = {
       "editor.tabSize" = 2;
       "files.trimTrailingWhitespace" = true;
-      "editor.fontSize" = 16;
+      "editor.fontSize" = 14;
+      "terminal.integrated.fontSize" = 14;
       "files.autoSave" = "onFocusChange";
       "editor.fontFamily" = "'JetBrains Mono', Menlo, Monaco, 'Courier New', monospace";
       "editor.fontLigatures" = true;
       "workbench.colorTheme" = "Catppuccin Mocha";
+      "editor.wordWrap" = "on";
     };
     userTasks = {};
     keybindings = [
