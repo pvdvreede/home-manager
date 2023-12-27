a59365aa902a9cbcfb343b24eb67368d9275ae9a
pvdvreede <pvdvreede@gmail.com>
2023-12-27 13:55:39 +1100

    fix gh auth read only file system bug.

--- common.nix
+++ common.nix
@@ -75,6 +75,10 @@
   programs.gh = {
     enable = true;
     gitCredentialHelper.enable = true;
+    settings = {
+      # Workaround for https://github.com/nix-community/home-manager/issues/4744
+      version = 1;
+    };
   };
 
   programs.fish = {
