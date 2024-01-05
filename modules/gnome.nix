eb37cdb3fc885d2754c645ac59e2df14d9448499
pvdvreede <pvdvreede@gmail.com>
2024-01-06 10:23:20 +1100

    more gnome tweaks.

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -20,20 +20,28 @@
       sleep-inactive-ac-type = "suspend";
       sleep-inactive-ac-timeout = (60 * 60 * 2);
     };
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-schedule-automatic = false;
+      night-light-schedule-from = 19.0;
+      night-light-schedule-to = 7.0;
+      night-light-temperature = 3000;
+    };
     "org/gnome/shell" = {
       enabled-extensions =
-        [ "pano@elhan.io" "Vitals@CoreCoding.com" "forge@jmmaranan.com" ];
+        [ "pano@elhan.io" "Vitals@CoreCoding.com" "forge@jmmaranan.com" "blur-my-shell@aunetx"];
     };
     "org/gnome/shell/extensions/forge" = {
       window-gap-hidden-on-single = false;
       window-gap-size = 8;
+      window-gap-size-increment = 2;
+      workspace-skip-tile = "";
     };
     "org/gtk/settings/file-chooser" = {
       clock-format = "24h";
     };
     "org/gnome/desktop/screensaver" = {
       lock-enabled = true;
-      lock-delay = 60;
+      lock-delay = 360;
     };
   };
 }
