3035f1ab745cf489d1c5f9f187fc13d61a0cf713
pvdvreede <pvdvreede@gmail.com>
2024-05-19 09:28:59 +1000

    add helix like matching to vscode.

--- modules/vscode/dance.nix
+++ modules/vscode/dance.nix
@@ -14,5 +14,31 @@
         };
       };
     };
+
+    keybindings = [
+      {
+        "key" = "m";
+        "command" = "dance.openMenu";
+        "when" = "editorTextFocus && dance.mode == 'normal'";
+        "args" = {
+          "menu" = {
+            "items" = {
+              "i" = {
+                "text" = "select inside";
+                "command" = "dance.seek.askObject.inner";
+              };
+              "a" = {
+                "text" = "select around";
+                "command" = "dance.seek.askObject";
+              };
+              "m" = {
+                "text" = "goto matching bracket";
+                "command" = "dance.seek.enclosing";
+              };
+            };
+          };
+        };
+      }
+    ];
   };
 }
