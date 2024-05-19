9b3816c9710b05acd3067855b2a387cf1436cd6a
pvdvreede <pvdvreede@gmail.com>
2024-05-19 14:53:20 +1000

    sort minimal vscode ui settings.

--- modules/vscode/minimalui.nix
+++ modules/vscode/minimalui.nix
@@ -10,25 +10,22 @@
 
     userSettings = {
       "breadcrumbs.enabled" = false;
-      "editor.minimap.enabled" = false;
-      "workbench.sideBar.location" = "right";
-      "workbench.activityBar.visible" = false;
-      "workbench.layoutControl.enabled" = false;
-      "editor.scrollbar.horizontal" = "hidden";
-      "editor.scrollbar.vertical" = "hidden";
-      "window.menuBarVisibility" = "toggle";
-      "workbench.editor.showTabs" = "none";
 
-      "terminal.integrated.fontSize" = 16;
 
       "editor.fontSize" = 16;
       "editor.fontFamily" = "JetBrains Mono Medium";
       "editor.fontLigatures" = true;
-      "editor.padding.top" = 15;
+      "editor.minimap.enabled" = false;
       "editor.padding.bottom" = 15;
+      "editor.padding.top" = 15;
+      "editor.scrollbar.horizontal" = "hidden";
+      "editor.scrollbar.vertical" = "hidden";
+
+      "terminal.integrated.fontSize" = 16;
 
       "window.zoomLevel" = 0.5;
 
+      "workbench.activityBar.visible" = false;
       "workbench.colorTheme" = "Atom One Dark";
       "workbench.colorCustomizations" = {
         "editorOverviewRuler.border" = "#0000";
@@ -44,7 +41,10 @@
         "editorOverviewRuler.warningForeground" = "#0000";
         "editorOverviewRuler.infoForeground" = "#0000";
         "editorOverviewRuler.bracketMatchForeground" = "#0000";
-    };
+      };
+      "workbench.editor.showTabs" = "none";
+      "workbench.layoutControl.enabled" = false;
+      "workbench.sideBar.location" = "right";
     };
   };
 }
