8befa17a111a278d76962c87dbe8489cb1f7240f
pvdvreede <pvdvreede@gmail.com>
2024-05-19 14:53:20 +1000

    sort minimal vscode ui settings.

--- modules/vscode/minimalui.nix
+++ modules/vscode/minimalui.nix
@@ -10,26 +10,26 @@
 
     userSettings = {
       "breadcrumbs.enabled" = false;
-      "editor.minimap.enabled" = false;
-      "workbench.sideBar.location" = "right";
-      "workbench.activityBar.visible" = false;
-      "workbench.layoutControl.enabled" = false;
-      "editor.scrollbar.horizontal" = "hidden";
-      "editor.scrollbar.vertical" = "hidden";
-      "window.menuBarVisibility" = "toggle";
-      "workbench.editor.showTabs" = "none";
-
-      "editor.fontSize" = 16;
-      "terminal.integrated.fontSize" = 16;
 
       "editor.fontFamily" = "JetBrains Mono Medium";
       "editor.fontLigatures" = true;
-      "editor.padding.top" = 15;
+      "editor.fontSize" = 16;
+      "editor.minimap.enabled" = false;
       "editor.padding.bottom" = 15;
+      "editor.padding.top" = 15;
+      "editor.scrollbar.horizontal" = "hidden";
+      "editor.scrollbar.vertical" = "hidden";
 
+      "terminal.integrated.fontSize" = 16;
+
+      "window.menuBarVisibility" = "toggle";
       "window.zoomLevel" = 0.5;
 
+      "workbench.activityBar.visible" = false;
       "workbench.colorTheme" = "Atom One Dark";
+      "workbench.editor.showTabs" = "none";
+      "workbench.layoutControl.enabled" = false;
+      "workbench.sideBar.location" = "right";
     };
   };
 }
