decef8d9b827d3c4ebec3590ab870c1d8b2065c2
pvdvreede <pvdvreede@gmail.com>
2024-08-23 17:20:51 +1000

    update packages and fix vscode font issues.

--- modules/vscode/theme.nix
+++ modules/vscode/theme.nix
@@ -4,12 +4,12 @@
   programs.vscode = {
     userSettings = {
       "editor.fontSize" = 14;
-      "terminal.integrated.fontSize" = 12;
+      "terminal.integrated.fontSize" = 14;
       "window.commandCenter" = false;
       "workbench.layoutControl.enabled" = false;
       "workbench.sideBar.location" = "right";
-      "editor.fontFamily" = "JetBrains Mono Regular";
-      "terminal.integrated.fontFamily" = "JetBrains Mono Regular";
+      "editor.fontFamily" = "JetBrains Mono";
+      "terminal.integrated.fontFamily" = "JetBrains Mono";
     };
   };
 }
