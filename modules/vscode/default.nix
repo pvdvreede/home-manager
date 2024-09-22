b2697c26b6f3c70895a1b72c2e0e33170d05eabc
pvdvreede <pvdvreede@gmail.com>
2024-09-22 09:18:28 +0000

    formatting vscode module.

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -1,11 +1,14 @@
-{ config, pkgs, vscode-marketplace, ... }:
-let
-  vscodeSettingsDir = if pkgs.system == "aarch64-darwin" then
-    "$HOME/Library/Application Support/Code/User"
-  else
-    "$HOME/.config/Code/User";
-in rec {
-  imports = [ ./editing.nix ./theme.nix ./keybindings.nix ];
+{
+  pkgs,
+  vscode-marketplace,
+  ...
+}: let
+  vscodeSettingsDir =
+    if pkgs.system == "aarch64-darwin"
+    then "$HOME/Library/Application Support/Code/User"
+    else "$HOME/.config/Code/User";
+in {
+  imports = [./editing.nix ./theme.nix ./keybindings.nix];
 
   programs.vscode = {
     enable = true;
@@ -27,36 +30,39 @@ in rec {
         jnoortheen.nix-ide
         bierner.markdown-mermaid
         elves.elvish
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-        name = "remote-containers";
-        publisher = "ms-vscode-remote";
-        version = "0.379.0";
-        sha256 = "ZXHWwmZDjzdyObpBFAWIML3B8fjByBgIa6Ej5Bm0N/Y=";
-      }];
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "remote-containers";
+          publisher = "ms-vscode-remote";
+          version = "0.379.0";
+          sha256 = "ZXHWwmZDjzdyObpBFAWIML3B8fjByBgIa6Ej5Bm0N/Y=";
+        }
+      ];
     userSettings = {
       "nix.serverSettings" = {
         "nil" = {
-          "diagnostics" = { "ignored" = [ ]; };
-          "formatting" = { "command" = [ "nixpkgs-fmt" ]; };
+          "diagnostics" = {"ignored" = [];};
+          "formatting" = {"command" = ["nixpkgs-fmt"];};
         };
       };
       "files.readonlyFromPermissions" = true;
       "terminal.integrated.profiles.osx" = {
         elvish = {
           path = "${pkgs.elvish}/bin/elvish";
-          args = [ ];
+          args = [];
         };
       };
       "terminal.integrated.profiles.linux" = {
         elvish = {
           path = "${pkgs.elvish}/bin/elvish";
-          args = [ ];
+          args = [];
         };
       };
       "terminal.integrated.defaultProfile.osx" = "elvish";
       "terminal.integrated.defaultProfile.linux" = "elvish";
     };
-    userTasks = { };
+    userTasks = {};
   };
 
   # The below 2 hooks are added to make the settings json file in vscode writable, as otherwise
@@ -67,8 +73,8 @@ in rec {
   # We need to remove the old copied settings.json (from the last home-manager switch) at this point so that home manager
   # does not error with 'an existing file is in the way'.
   home.activation.ignoreAnyExistingVsCodeSettings = {
-    after = [ ];
-    before = [ "checkLinkTargets" ];
+    after = [];
+    before = ["checkLinkTargets"];
     data = ''
       userDir="${vscodeSettingsDir}"
       rm -rf "$userDir/settings.json"
@@ -78,10 +84,9 @@ in rec {
   # after the settings json is rendered out and symlinked, then we can go in
   # and cat the settings json into a standard file in the VSCode dir.
   home.activation.makeVsCodeSettingsMutable = {
-    after = [ "writeBoundary" ];
-    before = [ ];
+    after = ["writeBoundary"];
+    before = [];
     data = ''
-
       userDir="${vscodeSettingsDir}"
       mv "$userDir/settings.json" "$userDir/settings.ln.json"
       cat "$userDir/settings.ln.json" | ${pkgs.jq}/bin/jq --monochrome-output > "$userDir/settings.json"
