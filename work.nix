8599ac802e322a64d4d06ed2be14bc5c987e4f03
pvdvreede <pvdvreede@gmail.com>
2024-09-23 11:53:02 +1000

    remove zsh setup for work laptop.

--- work.nix
+++ work.nix
@@ -1,21 +1,4 @@
-{ pkgs, ... }:
-
-{
-  home.file = {
-    zshrc = {
-      enable = true;
-      executable = true;
-      text = ''
-        #! /bin/sh
-
-        if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-          source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-        fi
-      '';
-      target = ".zshrc";
-    };
-  };
-
+{pkgs, ...}: {
   home.packages = with pkgs; [
     awscli2
     istioctl
