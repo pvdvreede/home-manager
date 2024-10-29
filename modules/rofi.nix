2067ca97a869fbf2f25480f9df18e753c8238304
pvdvreede <pvdvreede@gmail.com>
2024-10-29 17:15:27 +1100

    add power menu to rofi

--- modules/rofi.nix
+++ modules/rofi.nix
@@ -1,13 +1,12 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   config = {
-    home.packages = [ pkgs.jetbrains-mono ];
+    home.packages = [pkgs.jetbrains-mono pkgs.rofi-power-menu];
 
     programs.rofi = {
       enable = true;
       font = "JetBrains Mono";
       theme = "arthur";
+      plugins = [pkgs.rofi-power-menu];
       extraConfig = {
         modi = "drun,window,keys,filebrowser";
         window-thumbnail = true;
