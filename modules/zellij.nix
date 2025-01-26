13112f75ea1f31c42983355fbbcc48f1d505861a
pvdvreede <pvdvreede@gmail.com>
2025-01-26 09:09:03 +0000

    make sure fish is used for zellij default shell.

--- modules/zellij.nix
+++ modules/zellij.nix
@@ -1,41 +1,42 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   programs.zellij = {
     enable = true;
     settings = {
-      default_shell = "${pkgs.elvish}/bin/elvish";
+      default_shell = "${pkgs.fish}/bin/fish";
       default_layout = "compact";
       pane_frames = false;
       theme = "nord";
       keybinds = {
         "normal clear-defaults=true" = {
-          "bind \"Ctrl Space\"" = { SwitchToMode = "tmux"; };
-          "bind \"F12\"" = { SwitchToMode = "locked"; };
+          "bind \"Ctrl Space\"" = {SwitchToMode = "tmux";};
+          "bind \"F12\"" = {SwitchToMode = "locked";};
         };
-        locked = { "bind \"F12\"" = { SwitchToMode = "normal"; }; };
-        tmux = {
-          "bind \"o\"" = {
-            "LaunchOrFocusPlugin \"zellij:session-manager\"" = {
-              floating = true;
-            };
-            SwitchToMode = "normal";
-          };
-          "bind \"v\"" = {
-            NewPane = "Right";
-            SwitchToMode = "normal";
-          };
-          "bind \"s\"" = {
-            NewPane = "Down";
-            SwitchToMode = "normal";
-          };
-        } // (builtins.foldl' (acc: i:
+        locked = {"bind \"F12\"" = {SwitchToMode = "normal";};};
+        tmux =
           {
-            "bind \"${builtins.toString i}\"" = {
-              GoToTab = i;
+            "bind \"o\"" = {
+              "LaunchOrFocusPlugin \"zellij:session-manager\"" = {
+                floating = true;
+              };
               SwitchToMode = "normal";
             };
-          } // acc) { } [ 1 2 3 4 5 6 ]);
+            "bind \"v\"" = {
+              NewPane = "Right";
+              SwitchToMode = "normal";
+            };
+            "bind \"s\"" = {
+              NewPane = "Down";
+              SwitchToMode = "normal";
+            };
+          }
+          // (builtins.foldl' (acc: i:
+            {
+              "bind \"${builtins.toString i}\"" = {
+                GoToTab = i;
+                SwitchToMode = "normal";
+              };
+            }
+            // acc) {} [1 2 3 4 5 6]);
       };
     };
   };
