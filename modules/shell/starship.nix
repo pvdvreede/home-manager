c360020622e07c9b52eb0c2771fa7f415da9b4f6
pvdvreede <pvdvreede@gmail.com>
2025-03-02 06:56:46 +0000

    update startship theme to tokyo night.

--- modules/shell/starship.nix
+++ modules/shell/starship.nix
@@ -2,10 +2,36 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = "$all";
+      # format = "$all";
+      format = "[░▒▓](#a3aed2)(bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)\n$character";
       add_newline = true;
       container = {
         disabled = true;
+      };
+      directory = {
+        style = "fg:#e3e5e5 bg:#769ff0";
+        format = "[ $path ]($style)";
+        substitutions = {
+          "Documents" = "󰈙 ";
+          "Downloads" = " ";
+          "Music" = " ";
+          "Pictures" = " ";
+        };
+      };
+      git_branch = {
+        symbol = "";
+        style = "bg:#394260";
+        format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
+      };
+      git_status = {
+        style = "bg:#394260";
+        format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
+      };
+      time = {
+        disabled = false;
+        time_format = "%T";
+        style = "bg:#1d2230";
+        format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
       };
       character = {
         success_symbol = "[➜](bold green)";
