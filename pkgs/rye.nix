cbd2683a1469d2f395825c559bd079954ee60b13
pvdvreede <pvdvreede@gmail.com>
2025-03-04 09:25:46 +0000

    update rye package to be for linxu and darwin.

--- pkgs/rye.nix
+++ pkgs/rye.nix
@@ -8,13 +8,17 @@
     if stdenv.hostPlatform.parsed.cpu.name == "aarch64"
     then "arm64"
     else stdenv.hostPlatform.parsed.cpu.name;
+  sha = {
+    "x86_64-linux" = "sha256-BbT84ry/7zl8GI1dfl0ZC3/rDZizA/oZtOUKUqGjfLI=";
+    "aarch64-darwin" = "sha256-+a2rtLSnZcjzc5VbrBzUzS4G9btC/VtXQHUFu1CDkGw=";
+  };
 in
   stdenv.mkDerivation {
     pname = "rye";
     version = "${version}";
     src = fetchurl {
       url = "https://github.com/refaktor/rye/releases/download/${version}/rye_${os}_${arch}.tar.gz";
-      hash = "sha256-+a2rtLSnZcjzc5VbrBzUzS4G9btC/VtXQHUFu1CDkGw=";
+      hash = sha.${stdenv.hostPlatform.system};
     };
 
     unpackPhase = ''
