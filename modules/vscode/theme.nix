0448771d8a2051dc0b62c95ca91ffc60b77119ba
pvdvreede <pvdvreede@gmail.com>
2025-10-17 09:37:29 +1100

    changing to alabaster theme in vscode.

--- modules/vscode/theme.nix
+++ modules/vscode/theme.nix
@@ -1,10 +1,12 @@
 {vscode-marketplace, ...}: {
   programs.vscode = {
-    profiles.default.extensions = with vscode-marketplace; [akamud.vscode-theme-onedark];
+    profiles.default.extensions = with vscode-marketplace; [tonsky.theme-alabaster];
 
     profiles.default.userSettings = {
       "editor.fontSize" = 16;
       "terminal.integrated.fontSize" = 16;
+      "workbench.colorTheme" = "Alabaster";
+      "workbench.preferredLightColorTheme" = "Alabaster";
       "window.commandCenter" = false;
       "workbench.layoutControl.enabled" = false;
       "workbench.sideBar.location" = "right";
