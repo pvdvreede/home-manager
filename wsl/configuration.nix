98455e356f4d81b88542dba32ff6ff79e48799a0
pvdvreede <pvdvreede@gmail.com>
2025-11-10 08:39:37 +0000

    setup wsl with nixos to run rdp for window manager.

--- wsl/configuration.nix
+++ wsl/configuration.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   environment.systemPackages = [
     pkgs.wget
     pkgs.coreutils # needed for vscode devcontainers
@@ -12,6 +11,18 @@
   services.xserver.windowManager.i3.enable = true;
   services.xrdp.enable = true;
   services.xrdp.openFirewall = true;
+  services.xrdp.defaultWindowManager = ''
+    rm -rf /tmp/.X* /tmp/.X11-unix/*
+
+    export XDG_SESSION_TYPE=x11
+    export DISPLAY=:10
+    export LIBGL_ALWAYS_INDIRECT=1
+    export PATH="${pkgs.i3}/bin:$PATH"
+
+    # Start i3 directly
+    # exec ${pkgs.xorg.xinit}/bin/startx
+    exec i3
+  '';
 
   programs.nix-ld = {
     enable = true;
@@ -24,6 +35,7 @@
 
   users.users.nixos = {
     isNormalUser = true;
+    hashedPassword = "$6$hepjXE61HxWzfEbl$NF.ujvhEopLv8Ce54mfVuS44g3iUdJ9pD7iUBqOuIPYAjQJ8diQSaqkorNC.ZUJPTdkU0.C9uIG1c/tv.j9qt1";
     extraGroups = [
       "wheel"
       "docker"
