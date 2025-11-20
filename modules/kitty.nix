8c31703e7a1f40feb13409ee7eaa553736c5ed48
pvdvreede <pvdvreede@gmail.com>
2025-11-21 10:31:58 +1100

    add keybinds config for kitty.

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -9,6 +9,12 @@
       disable_ligatures = "never";
       window_padding_width = 2;
       confirm_os_window_close = -1;
+      remember_window_state = "yes";
+    };
+    keybindings = {
+      "ctrl+shift+n" = "new_tab_with_cwd";
+      "ctrl+shift+h" = "previous_tab";
+      "ctrl+shift+l" = "next_tab";
     };
   };
 }
