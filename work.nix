f68a166228241be7e651c51064a9d659831d89f5
pvdvreede <pvdvreede@gmail.com>
2025-11-20 11:21:15 +1100

    remove work packages that are installed elsewhere.

--- work.nix
+++ work.nix
@@ -1,15 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    awscli2
-    istioctl
-    jq
-    kubectl
-    kubernetes-helm
-    saml2aws
-    step-cli
     docker-compose
     docker-client
-    podman
     lima
   ];
 }
