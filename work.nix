356834e0c04b13e36264ca9f27bbac2e670aaf5e
pvdvreede <pvdvreede@gmail.com>
2026-01-08 10:52:14 +1100

    add ai for work vscode.

--- work.nix
+++ work.nix
@@ -1,7 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  vscode-marketplace,
+  ...
+}: {
   home.packages = with pkgs; [
     docker-compose
     docker-client
     lima
+  ];
+
+  programs.vscode.profiles.default.extensions = with vscode-marketplace; [
+    sourcegraph.amp
+    anthropic.claude-code
   ];
 }
