daa67cbd743ae9d2f51234c9a1bd566603277a16
pvdvreede <pvdvreede@gmail.com>
2026-02-22 04:20:36 +0000

    add in some basic home modules.

---
+++ modules/homeModules/jujutsu.nix
@@ -0,0 +1,18 @@
+{...}: {
+  flake.homeModules.jujutsu = {pkgs, ...}: {
+    programs.jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          email = "pvdvreede@gmail.com";
+          name = "pvdvreede";
+        };
+        remotes = {
+          origin.auto-track-bookmarks = "glob:*";
+        };
+      };
+    };
+
+    home.packages = [pkgs.jjui];
+  };
+}
