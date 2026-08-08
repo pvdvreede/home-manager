26379c01503c6f7a57bdd8589d016fbb9a2188ae
pvdvreede <pvdvreede@gmail.com>
2026-08-08 15:46:04 +1000

    update kitty config.

--- modules/homeModules/kitty.nix
+++ modules/homeModules/kitty.nix
@@ -6,13 +6,17 @@
       environment = {};
       font.name = "JetBrains Mono";
       font.package = pkgs.jetbrains-mono;
-      font.size = 16;
+      font.size = 14;
       settings = {
         allow_remote_control = "yes";
         disable_ligatures = "never";
-        window_padding_width = 2;
+        window_padding_width = 0;
         confirm_os_window_close = -1;
         remember_window_state = "yes";
+        tab_bar_edge = "top";
+        tab_bar_style = "powerline";
+        window_margin_width = 5;
+        window_border_width = "2pt";
       };
       keybindings = {
         "ctrl+shift+n" = "new_tab_with_cwd";
@@ -22,6 +26,7 @@
         "ctrl+shift+p" = "command_palette";
         "ctrl+h" = "previous_window";
         "ctrl+l" = "next_window";
+        "ctrl+shift+q" = "next_layout";
       };
     };
   };
