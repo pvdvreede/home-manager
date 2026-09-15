8d6c87f13d9dd1902a270021d8c6d218445625fd
pvdvreede <pvdvreede@gmail.com>
2026-09-15 10:31:25 +1000

    set xps-13 to use deep sleep.

--- modules/hosts/xps-13/_hardware-configuration.nix
+++ modules/hosts/xps-13/_hardware-configuration.nix
@@ -49,5 +49,5 @@
 
   # May help if FFmpeg/VAAPI/QSV init fails (esp. on Arc with i915):
   hardware.enableRedistributableFirmware = true;
-  boot.kernelParams = ["i915.enable_guc=3"];
+  boot.kernelParams = ["mem_sleep_default=deep" "i915.enable_guc=3"];
 }
